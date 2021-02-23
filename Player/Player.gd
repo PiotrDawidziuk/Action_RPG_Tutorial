8997e98d8ff00cddd13cf4322cc6b00d2d6dfb35
PiotrDawidziuk <piotr.k.dawidziuk@gmail.com>
2021-02-23 13:31:31 +0100

    movement animation

--- Player/Player.gd
+++ Player/Player.gd
@@ -6,6 +6,10 @@ const FRICTION = 500
 
 var velocity = Vector2.ZERO
 
+onready var animationPlayer = $AnimationPlayer
+onready var animationTree = $AnimationTree
+onready var animationState = animationTree.get("parameters/playback")
+
 func _physics_process(delta):
 	var input_vector = Vector2.ZERO
 	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
@@ -13,8 +17,12 @@ func _physics_process(delta):
 	input_vector = input_vector.normalized()
 
 	if input_vector != Vector2.ZERO:
+		animationTree.set("parameters/Idle/blend_position", input_vector)
+		animationTree.set("parameters/Run/blend_position", input_vector)
+		animationState.travel("Run")
 		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
 	else: 
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
-
+		animationState.travel("Idle")
+		
 	velocity = move_and_slide(velocity)
